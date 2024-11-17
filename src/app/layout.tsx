7d27877e14d65b306c9187d96bb802b242173aa6
lebaotai <fnight644@gmail.com>
2024-11-17 21:37:10 +0700

    add meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
   return (
     <AppProvider>
       <html lang="en" suppressHydrationWarning>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"></meta>
         <body className={`${SQUARE.className} antialiased`}>{children}</body>
       </html>
     </AppProvider>
