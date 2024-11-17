342e000faba7195afb4080eba0f6b4bfbf8d5e52
lebaotai <fnight644@gmail.com>
2024-11-18 01:30:54 +0700

    update ecosystem

--- src/app/ecosystem/[system]/page.tsx
+++ src/app/ecosystem/[system]/page.tsx
@@ -1,20 +1,15 @@
-import { getEcosystemData } from '@/api/ecosystem/ecosystem'
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import React from 'react'
 
-export default async function EcosystemDetails(props: any) {
-  const { params } = props
-
-  const data = await getEcosystemData(params.system)
-
+export default async function EcosystemDetails() {
   return (
     <div className="flex h-screen flex-col">
       <div className="">
         <Header />
       </div>
       <div className="flex flex-grow items-center justify-center">
-        <p className="text-center text-3xl font-bold">{data.ten}</p>
+        {/* <p className="text-center text-3xl font-bold">{data.ten}</p> */}
       </div>
       <div className="">
         <Footer />
