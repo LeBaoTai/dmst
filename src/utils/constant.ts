32c32dd3c1acc98e8cfbb6bf908399da924c2c5e
lebaotai <fnight644@gmail.com>
2024-11-18 11:23:31 +0700

    change api path and url

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -6,13 +6,13 @@ export const SQUARE = localFont({
   weight: '100 900'
 })
 
-// export const API_ROOT = 'http://116.109.42.111'
-export const API_ROOT = 'http://172.30.10.85'
+export const API_ROOT = 'http://116.109.42.111'
+// export const API_ROOT = 'http://172.30.10.85'
 export const API_PORT = '1337'
 
 export const DMST_ROOT_PAHT = '/api/dmst-trang-chu'
 
 
-// export const NEXT_PUBLIC_BASE_URL = 'https://dmst-eta.vercel.app'
+export const NEXT_PUBLIC_BASE_URL = 'https://dmst-eta.vercel.app'
 // export const NEXT_PUBLIC_BASE_URL = 'http:localhost:3000'
-export const NEXT_PUBLIC_BASE_URL = `${API_ROOT}:${API_PORT}`
+// export const NEXT_PUBLIC_BASE_URL = `${API_ROOT}:${API_PORT}`
